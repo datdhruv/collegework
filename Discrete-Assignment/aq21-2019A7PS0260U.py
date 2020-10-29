118d4340840c6dacfce5a16543a852c58b85e038
Dhruv <Dhruv>
2020-10-29 08:59:27 +0000

    something

--- Discrete-Assignment/aq21-2019A7PS0260U.py
+++ Discrete-Assignment/aq21-2019A7PS0260U.py
@@ -1,1 +1,33 @@
 # Q21. Given a graph and a positive integer n, write a program to determine whether the graph has a cycle of length n.
+
+''' Here is the plan bruv
+- we need to implement a dfs generator, which will create all possible paths
+  - this will be stored in a dict/list
+  - doing this using a linked list
+- then we will filter based on length
+- and finally from the above step we filter based on whether it has repeated edges and vertices or not (thoda unclear hai here)
+'''
+graph = [[0, 1, 0, 1, 0], 
+         [1 ,0 ,1 ,0, 1], 
+         [0, 1, 0, 1, 0], 
+         [1, 0, 1, 0, 1], 
+         [0, 1, 0, 1, 0]] 
+
+class Node:
+    nxt = None
+    prev = None
+    curr = None
+
+node1 = Node()
+
+def DFS(graph : list, node : int):
+
+    Node_List = [0 for i in range(len(graph))]
+
+    for interation in range(len(graph)):
+        Node_List[iteration] = Node()
+        for i in iteration:
+            if i == 1:
+                                Node_Lis
+
+
