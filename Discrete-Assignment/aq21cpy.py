05fc78f604331eb0bf59ea844937a792de88c9fc
Dhruv <Dhruv>
2020-11-01 14:35:35 +0000

    check

--- Discrete-Assignment/aq21cpy.py
+++ Discrete-Assignment/aq21cpy.py
@@ -1,46 +1,61 @@
-def DFS(graph, marked, n, vert, start, count): 
+def DFS(graph, visited, n, v, start, count): 
 
-	marked[vert] = True
+	visited[v] = True
 
 	if n == 0: 
-		marked[vert] = False
-		if graph[vert][start] == 1: 
+		visited[v] = False
+		if graph[v][start] == 1: 
 			count = count + 1
 			return count 
 		else: 
 			return count 
 
 	for i in range(V): 
-		if marked[i] == False and graph[vert][i] == 1: 
-			count = DFS(graph, marked, n-1, i, start, count) 
-	marked[vert] = False
+		if visited[i] == False and graph[v][i] == 1: 
+			count = DFS(graph, visited, n-1, i, start, count) 
+	visited[v] = False
 	return count 
 
-def checkCycle( graph, n): 
+def isCycle( graph, n): 
 
-    marked = [False] * V 
+    visited = [False] * V 
 
     count = 0
     for i in range(V-(n-1)): 
-        count = DFS(graph, marked, n-1, i, i, count) 
+        count = DFS(graph, visited, n-1, i, i, count) 
 
-        marked[i] = True
+        visited[i] = True
 	
         if count > 0:
             return True
         else:
             return False
 
-graph = [[0, 1, 0, 1, 0], 
-        [1 ,0 ,1 ,0, 1], 
-	[0, 1, 0, 1, 0], 
-	[1, 0, 1, 0, 1], 
-	[0, 1, 0, 1, 0]] 
-			
 
-V = len(graph[0])
-n = int(input("Enter the number of cycles to check "))
 
-print("Is there a cycle of length ",n," in the provided graph? ",checkCycle(graph, n)) 
+with open('aq21ip-2019A7PS0260U.txt','r') as f:
+    a = '0'
+    b = '0'
+    graph = []
+    while b != 'Terminate' or a!='':
+        while a != '\n': 
+            a = f.readline()
+            graph.append(a.strip().split(sep=','))
+        graph.pop()
+        n = f.readline()
+    
+        for i in range(len(graph)):
+            for j in range(len(graph[i])):
+                graph[i][j] = int(graph[i][j])
+
+        V = len(graph[0])
+
+        print("Is there a cycle of length ",n," in the provided graph? ",isCycle(graph, int(n))) 
+       
+        a = '0'
+        b = f.readline()
+
+
+
 
 
